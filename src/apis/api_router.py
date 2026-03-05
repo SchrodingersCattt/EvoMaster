6b7bafb4299f7e708e71c3a61e08b9abaf0b0b23
zhouh@dp.tech <zhouh@dp.tech>
2026-03-05 22:26:50 +0800

    feat：添加memory调试api以支持tracemalloc快照和增量分析

--- src/apis/api_router.py
+++ src/apis/api_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from src.apis import chat_api
+from src.apis import chat_api, debug_api
 
 api_router = APIRouter()
 api_router.include_router(chat_api.router, prefix='/chat/sessions')
+api_router.include_router(debug_api.router, prefix='/debug')
