dc5290536b3ba6dd1f9b011d4e6a0e944953d43e
zhouh@dp.tech <zhouh@dp.tech>
2026-03-22 15:38:20 +0800

    feat：增加格式化展示模型信息功能以改善通知内容显示

--- src/services/stream_service.py
+++ src/services/stream_service.py
@@ -35,6 +35,7 @@ from src.services.worker_registry_service import get_worker_registry_service
 from src.utils.constant import AG_UI_EVENT, CURRENT_ENV, REDIS_URL
 from src.utils.feishu_notifier import (
     CARD_TEMPLATE_ORANGE,
+    format_llm_model_for_notify,
     notify_post_async,
 )
 from src.utils.worker_id import get_worker_id
@@ -791,6 +792,7 @@ class ChatStreamService:
                         ('会话ID', sid),
                         ('会话地址', session_url),
                         ('用户', user_info_display),
+                        ('模型', format_llm_model_for_notify(ctx.llm, ctx.model)),
                         ('用户问题', user_question or '-'),
                         ('排队数', str(queue_len)),
                         ('执行中', str(active_count)),
