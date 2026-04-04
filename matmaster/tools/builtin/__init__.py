dc7959b74818e5cf5f4029ccd2afcc367f4b4554
lidongzhi@dp.tech <159016867098@163.com>
2026-04-04 19:39:05 +0800

    Revert "feat(tools): add agentic builtin tools"
    This reverts commit 13d284e5786b409eef7b58ae7887c412fd7e520e.

--- matmaster/tools/builtin/__init__.py
+++ matmaster/tools/builtin/__init__.py
@@ -6,26 +6,20 @@ All tools inherit from BuiltinTool ABC and satisfy the Tool Protocol.
 Tools are added incrementally by plan-01 through plan-04.
 """
 
-from matmaster.tools.builtin.agent_tool import AgentTool
 from matmaster.tools.builtin.base import BuiltinTool
 from matmaster.tools.builtin.bash_tool import BashTool
 from matmaster.tools.builtin.edit_tool import EditTool
 from matmaster.tools.builtin.glob_tool import GlobTool
 from matmaster.tools.builtin.grep_tool import GrepTool
 from matmaster.tools.builtin.read_tool import ReadTool
-from matmaster.tools.builtin.skill_tool import SkillTool
-from matmaster.tools.builtin.todo_write_tool import TodoWriteTool
 from matmaster.tools.builtin.write_tool import WriteTool
 
 __all__ = [
-    "AgentTool",
     "BuiltinTool",
     "BashTool",
     "EditTool",
     "GlobTool",
     "GrepTool",
     "ReadTool",
-    "SkillTool",
-    "TodoWriteTool",
     "WriteTool",
 ]
